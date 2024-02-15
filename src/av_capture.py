17a6a53a9bb83b032ab6e0cac4ca75253b774490
yardencsGitHub <yardencsmail@gmail.com>
2024-02-15 13:20:25 +0200

    Update av_capture.py

--- src/av_capture.py
+++ src/av_capture.py
@@ -2,19 +2,7 @@
 -------------------------------------------------------------------------
 Yarden Cohen, Feb 2024
 
-This script runs in a terminal and can replace the VideoCapture app (https://github.com/gardner-lab/video-capture) if needed. 
-The FinchScope (https://github.com/gardner-lab/FinchScope) was developed by Will Liberti for capturing fluorescence signals in small animals.
-The script monitors a microphone, detects events based on their spectral acoustic properties, and uses these events to trigger turning on the FinchScope's 
-LED and to synchronously acquire the microphone and FinchScope fluorescence video. 
-
-Installation:
-- Create a conda environment with the latest python and the following packages:
-ffmpeg, matplotlib, numpy, scipy, pandas, tkinter, pyserial, pyaudio, opencv-python, json
-
-
-Usage:
-
-Canary song triggering """ 
+ """ 
 
 import concurrent.futures
 import pyaudio
